92920869be0ebdbac32a70a867e85a9bac22f64b
vinikis <viniciusqueiroga@live.com>
2016-09-17 02:29:53 -0300

    Inclusão da função importar excel na tela eleitores

--- prjEleicao/Forms/frmEleitor.Designer.cs
+++ prjEleicao/Forms/frmEleitor.Designer.cs
@@ -32,9 +32,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEleitor));
             this.label6 = new System.Windows.Forms.Label();
             this.pnlTopo = new System.Windows.Forms.Panel();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.txtPesq = new System.Windows.Forms.TextBox();
+            this.btnExcluir = new System.Windows.Forms.Button();
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.lstView = new System.Windows.Forms.ListBox();
             this.txtNome = new System.Windows.Forms.TextBox();
@@ -43,13 +47,11 @@
             this.asyncEditar = new System.ComponentModel.BackgroundWorker();
             this.asyncExcluir = new System.ComponentModel.BackgroundWorker();
             this.txtCPF = new System.Windows.Forms.MaskedTextBox();
-            this.btnImport = new System.Windows.Forms.Button();
-            this.btnEdit = new System.Windows.Forms.Button();
-            this.btnExcluir = new System.Windows.Forms.Button();
-            this.btnAdd = new System.Windows.Forms.Button();
             this.pcbStatus = new System.Windows.Forms.PictureBox();
             this.tpInfo = new System.Windows.Forms.ToolTip(this.components);
             this.tmPesq = new System.Windows.Forms.Timer(this.components);
+            this.asyncImportarExcel = new System.ComponentModel.BackgroundWorker();
+            this.pbarStatus = new System.Windows.Forms.ProgressBar();
             this.pnlTopo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pcbStatus)).BeginInit();
             this.SuspendLayout();
@@ -86,6 +88,44 @@
             this.pnlTopo.TabIndex = 58;
             this.pnlTopo.Tag = "editable";
             // 
+            // btnImport
+            // 
+            this.btnImport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.btnImport.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnImport.BackgroundImage")));
+            this.btnImport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnImport.FlatAppearance.BorderSize = 0;
+            this.btnImport.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(105)))), ((int)(((byte)(92)))));
+            this.btnImport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImport.Font = new System.Drawing.Font("Segoe UI Light", 9.25F);
+            this.btnImport.ForeColor = System.Drawing.Color.White;
+            this.btnImport.Location = new System.Drawing.Point(189, 27);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(38, 30);
+            this.btnImport.TabIndex = 35;
+            this.btnImport.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.tpInfo.SetToolTip(this.btnImport, "Importar eleitores");
+            this.btnImport.UseVisualStyleBackColor = false;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(169)))), ((int)(((byte)(244)))));
+            this.btnEdit.BackgroundImage = global::prjEleicao.Properties.Resources.edit;
+            this.btnEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnEdit.FlatAppearance.BorderSize = 0;
+            this.btnEdit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
+            this.btnEdit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(182)))), ((int)(((byte)(246)))));
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI Light", 9.25F);
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Location = new System.Drawing.Point(341, 69);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(36, 30);
+            this.btnEdit.TabIndex = 22;
+            this.tpInfo.SetToolTip(this.btnEdit, "Editar");
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
             // btnSave
             // 
             this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
@@ -118,6 +158,27 @@
             this.txtPesq.TabIndex = 23;
             this.txtPesq.TextChanged += new System.EventHandler(this.txtPesq_TextChanged);
             // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExcluir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(67)))), ((int)(((byte)(54)))));
+            this.btnExcluir.BackgroundImage = global::prjEleicao.Properties.Resources.excluir;
+            this.btnExcluir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnExcluir.FlatAppearance.BorderSize = 0;
+            this.btnExcluir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Brown;
+            this.btnExcluir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.Font = new System.Drawing.Font("Segoe UI Light", 9.25F);
+            this.btnExcluir.ForeColor = System.Drawing.Color.White;
+            this.btnExcluir.Location = new System.Drawing.Point(682, 69);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(30, 30);
+            this.btnExcluir.TabIndex = 21;
+            this.btnExcluir.Tag = "";
+            this.tpInfo.SetToolTip(this.btnExcluir, "Excluir");
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
@@ -128,6 +189,25 @@
             this.lblTitulo.Size = new System.Drawing.Size(152, 46);
             this.lblTitulo.TabIndex = 26;
             this.lblTitulo.Text = "Eleitores";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.btnAdd.BackgroundImage = global::prjEleicao.Properties.Resources.adicionar;
+            this.btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(105)))), ((int)(((byte)(92)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI Light", 9.25F);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(231, 27);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(64, 30);
+            this.btnAdd.TabIndex = 20;
+            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.tpInfo.SetToolTip(this.btnAdd, "Adicionar");
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnCancel
             // 
@@ -216,84 +296,6 @@
             this.txtCPF.TabIndex = 64;
             this.txtCPF.Tag = "editable";
             // 
-            // btnImport
-            // 
-            this.btnImport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
-            this.btnImport.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnImport.BackgroundImage")));
-            this.btnImport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnImport.FlatAppearance.BorderSize = 0;
-            this.btnImport.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(105)))), ((int)(((byte)(92)))));
-            this.btnImport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnImport.Font = new System.Drawing.Font("Segoe UI Light", 9.25F);
-            this.btnImport.ForeColor = System.Drawing.Color.White;
-            this.btnImport.Location = new System.Drawing.Point(189, 27);
-            this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(38, 30);
-            this.btnImport.TabIndex = 35;
-            this.btnImport.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
-            this.tpInfo.SetToolTip(this.btnImport, "Importar eleitores");
-            this.btnImport.UseVisualStyleBackColor = false;
-            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
-            // 
-            // btnEdit
-            // 
-            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(169)))), ((int)(((byte)(244)))));
-            this.btnEdit.BackgroundImage = global::prjEleicao.Properties.Resources.edit;
-            this.btnEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnEdit.FlatAppearance.BorderSize = 0;
-            this.btnEdit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
-            this.btnEdit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(182)))), ((int)(((byte)(246)))));
-            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEdit.Font = new System.Drawing.Font("Segoe UI Light", 9.25F);
-            this.btnEdit.ForeColor = System.Drawing.Color.White;
-            this.btnEdit.Location = new System.Drawing.Point(341, 69);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(36, 30);
-            this.btnEdit.TabIndex = 22;
-            this.tpInfo.SetToolTip(this.btnEdit, "Editar");
-            this.btnEdit.UseVisualStyleBackColor = false;
-            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
-            // 
-            // btnExcluir
-            // 
-            this.btnExcluir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExcluir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(67)))), ((int)(((byte)(54)))));
-            this.btnExcluir.BackgroundImage = global::prjEleicao.Properties.Resources.excluir;
-            this.btnExcluir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnExcluir.FlatAppearance.BorderSize = 0;
-            this.btnExcluir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Brown;
-            this.btnExcluir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExcluir.Font = new System.Drawing.Font("Segoe UI Light", 9.25F);
-            this.btnExcluir.ForeColor = System.Drawing.Color.White;
-            this.btnExcluir.Location = new System.Drawing.Point(682, 69);
-            this.btnExcluir.Name = "btnExcluir";
-            this.btnExcluir.Size = new System.Drawing.Size(30, 30);
-            this.btnExcluir.TabIndex = 21;
-            this.btnExcluir.Tag = "";
-            this.tpInfo.SetToolTip(this.btnExcluir, "Excluir");
-            this.btnExcluir.UseVisualStyleBackColor = false;
-            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
-            this.btnAdd.BackgroundImage = global::prjEleicao.Properties.Resources.adicionar;
-            this.btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnAdd.FlatAppearance.BorderSize = 0;
-            this.btnAdd.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(105)))), ((int)(((byte)(92)))));
-            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAdd.Font = new System.Drawing.Font("Segoe UI Light", 9.25F);
-            this.btnAdd.ForeColor = System.Drawing.Color.White;
-            this.btnAdd.Location = new System.Drawing.Point(231, 27);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(64, 30);
-            this.btnAdd.TabIndex = 20;
-            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
-            this.tpInfo.SetToolTip(this.btnAdd, "Adicionar");
-            this.btnAdd.UseVisualStyleBackColor = false;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
             // pcbStatus
             // 
             this.pcbStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
@@ -317,12 +319,29 @@
             this.tmPesq.Interval = 1000;
             this.tmPesq.Tick += new System.EventHandler(this.tmPesq_Tick);
             // 
+            // asyncImportarExcel
+            // 
+            this.asyncImportarExcel.DoWork += new System.ComponentModel.DoWorkEventHandler(this.asyncImportarExcel_DoWork);
+            this.asyncImportarExcel.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.asyncImportarExcel_RunWorkerCompleted);
+            // 
+            // pbarStatus
+            // 
+            this.pbarStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbarStatus.Location = new System.Drawing.Point(-1, 641);
+            this.pbarStatus.Maximum = 0;
+            this.pbarStatus.Name = "pbarStatus";
+            this.pbarStatus.Size = new System.Drawing.Size(1011, 43);
+            this.pbarStatus.TabIndex = 65;
+            this.pbarStatus.Visible = false;
+            // 
             // frmEleitor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(202)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(1010, 683);
+            this.Controls.Add(this.pbarStatus);
             this.Controls.Add(this.txtCPF);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.pnlTopo);
@@ -363,6 +382,8 @@
         private System.Windows.Forms.Button btnImport;
         private System.Windows.Forms.ToolTip tpInfo;
         private System.Windows.Forms.Timer tmPesq;
+        private System.ComponentModel.BackgroundWorker asyncImportarExcel;
+        protected System.Windows.Forms.ProgressBar pbarStatus;
 
     }
 }
--- prjEleicao/Forms/frmEleitor.cs
+++ prjEleicao/Forms/frmEleitor.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
-using System.Data.OleDb;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
 
 namespace prjEleicao
 {
@@ -144,6 +145,108 @@ namespace prjEleicao
             Eleitor.Excluir(codigo);
         }
 
+        //IMPORTAR EXCEL
+        private void asyncImportarExcel_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string filepath = (string)e.Argument;
+
+            //instancia da interoperabilidade do Excel
+            Excel.Application app = new Excel.Application();//Aplicação
+            Excel.Workbook workbook = app.Workbooks.Open(filepath);//Documento
+            Excel.Worksheet worksheet = workbook.Worksheets[1];//Planilha
+
+            //contagem de erros de entradas
+            int entradasInvalidas = 0;
+
+            //barra de progresso
+            pbarStatus.BeginInvoke((MethodInvoker)delegate
+            {
+                pbarStatus.Value = 0;
+                pbarStatus.Maximum = worksheet.UsedRange.Rows.Count;
+                pbarStatus.Visible = true;
+            });
+
+            for (int i = 1; i <= worksheet.UsedRange.Rows.Count; i++)
+            {
+                string nome = rgx.Replace(worksheet.Cells[i, 1].Value, "");
+                string cpf = worksheet.Cells[i, 2].Value.Replace(" ", "").Replace(".", "");
+                int tryparseResult = 0;
+
+                if (nome == "")
+                {
+                    entradasInvalidas++;
+                    pbarStatus.BeginInvoke((MethodInvoker)delegate
+                    {
+                        pbarStatus.Value++;
+                    });
+                    continue;
+                }
+
+                if (cpf.Length < 12 || !cpf.Contains("-") || int.TryParse(cpf.Replace("-", ""), out tryparseResult))
+                {
+                    entradasInvalidas++;
+                    pbarStatus.BeginInvoke((MethodInvoker)delegate
+                    {
+                        pbarStatus.Value++;
+                    });
+                    continue;
+                }
+
+                Eleitor novoeleitor = new Eleitor(0, nome, cpf);
+                novoeleitor.Salvar();
+
+                pbarStatus.BeginInvoke((MethodInvoker)delegate
+                {
+                    pbarStatus.Value++;
+                });
+            }
+
+            pbarStatus.BeginInvoke((MethodInvoker)delegate
+            {
+                pbarStatus.Visible = false;
+            });
+
+            if (entradasInvalidas > 0)
+            {
+                e.Result = "Foram ignoradas " + entradasInvalidas.ToString() + " entradas inválidas.";
+            }
+
+            workbook.Close(false, null, null);
+
+            app.Quit();
+
+            Marshal.ReleaseComObject(workbook);
+            Marshal.ReleaseComObject(app);
+
+            worksheet = null;
+            workbook = null;
+            app = null;
+        }
+
+        private void asyncImportarExcel_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                asyncConsultar.RunWorkerAsync();
+                if (e.Result == null)
+                {
+                    Aviso.Mensagem("Importação concluída com sucesso!");
+                }
+                else
+                {
+                    Aviso.Alerta((string)e.Result);
+                }
+
+            }
+            else
+            {
+                pcbStatus.Visible = false;
+
+                Exception erro = e.Error;
+                Aviso.Erro(erro.Message);
+            }
+        }
+
         #endregion
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -228,35 +331,17 @@ namespace prjEleicao
         }
 
         private void btnImport_Click(object sender, EventArgs e)
-        {
-            /*
+        {   
             Aviso.Mensagem("Para importar dados de uma planilha Excel corretamente, coloque os NOMES na coluna A e os CPFs na coluna B.");
 
             OpenFileDialog planilha = new OpenFileDialog();
             planilha.Filter = "Planilha Excel 2007 ou superior | *.xlsx";
 
             if (planilha.ShowDialog() == DialogResult.OK) {
-
-                OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + planilha.FileName + ";Extended Properties=’Excel 12.0 Xml;HDR=YES’;");
-
-                try
-                {
-                    con.Open();    
-                }
-                catch (Exception ex) {
-                    Aviso.Erro(ex.Message);
-                    return;
-                }
-
-                OleDbCommand consultar = new OleDbCommand("SELECT * from [Sheet1$];", con);
-                OleDbDataReader dados = consultar.ExecuteReader();
-
-                while (dados.Read()) {
-                    MessageBox.Show(dados[0].ToString());
-                }
-
-            }
-            */
+                pcbStatus.Visible = true;
+                asyncImportarExcel.RunWorkerAsync(planilha.FileName);
+            }  
         }
+
     }
 }
